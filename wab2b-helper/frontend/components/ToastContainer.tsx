b183ffb08037caf8b75483ce63aaad242e13205e
Asdmir786 <asmir.alams.com@gmail.com>
2025-07-14 22:02:54 +0500

    Resetting

--- wab2b-helper/frontend/components/ToastContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ToastContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <div className="fixed bottom-0 right-0 p-4 z-50 space-y-4">
-      {children}
-    </div>
-  );
-}; 
